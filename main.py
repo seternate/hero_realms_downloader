39e8ae793325c82f6bebc98956210f554509ef52
seternate <seternate@gmail.com>
2018-10-01 07:53:45 +0200

    first push

---
+++ main.py
@@ -0,0 +1,17 @@
+import requests
+from bs4 import BeautifulSoup
+
+r = requests.get('https://www.herorealms.com/card-gallery/')
+soup = BeautifulSoup(r.text, features='html.parser')
+r.close()
+
+
+for image in soup.find_all('img'):
+    r = requests.get(image.get('src'), stream=True)
+    path = '.../Cards/' + image.get('src').rsplit('/', 1)[-1]
+    if r.status_code == 200:
+        with open(path, 'wb') as file:
+            file.write(r.content)
+
+print("awd")
+print("hallo")
