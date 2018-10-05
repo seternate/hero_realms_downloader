edcd2667e4336c5d03485601203ff32a1e35d01b
Levin <seternate@gmail.com>
2018-10-05 14:44:22 +0200

    push

--- data.py
+++ data.py
@@ -1,3 +1,7 @@
+import xml.etree.ElementTree as Et
+from xml.dom import minidom
+
+
 PATH = 'Cards/' + 'data.txt'
 FILE = open(PATH, 'r', encoding='utf-8')
 data = FILE.read()
@@ -6,14 +10,18 @@ FILE.close()
 
 
 matrix = []
-for entry in data.split('\n'):
+for idx, entry in enumerate(data.split('\n')):
     items = entry.split(';')
     card = {'image': items[0]}
     items[0] = items[0].split(',')
     items[2] = items[2].split('◆')
+    items[6] = items[6].split()
     for index in range(len(items[2])):
         items[2][index] = items[2][index].strip()
-    card = {'image': items[0],
+    for index in range(len(items[6])):
+        items[6][index] = items[6][index].strip()
+    card = {'id': str(idx),
+            'image': items[0],
             'name': items[1],
             'type': items[2],
             'text': items[3],
@@ -25,14 +33,51 @@ for entry in data.split('\n'):
             'setname': items[9],
             'setqty': items[10]}
     matrix.append(card)
-print(matrix)
 
 
-FILE = open('Cards/BAS-EN-001-arkus-imperial-dragon.jpg', 'rb')
-pic = FILE.read()
-FILE.close()
+root = Et.Element('herorealms')
+deck1 = Et.SubElement(root, 'deck', name='Basis Deck')
+for element in matrix:
+    card = Et.SubElement(deck1, 'card', id=element['id'], name=element['name'])
+    images = Et.SubElement(card, 'images')
+    for image in element['image']:
+        imageXml = Et.SubElement(images, 'image')
+        if image != 'None':
+            imageXml.text = image
+    types = Et.SubElement(card, 'types')
+    for type in element['type']:
+        typeXml = Et.SubElement(types, 'type')
+        if type != 'None':
+            typeXml.text = type
+    text = Et.SubElement(card, 'text')
+    if element['text'] != 'None':
+        text.text = element['text']
+    faction = Et.SubElement(card, 'faction')
+    if element['faction'] != 'None':
+        faction.text = element['faction']
+    cost = Et.SubElement(card, 'cost')
+    if element['cost'] != 'None':
+        cost.text = element['cost']
+    if len(element['defense']) is 1:
+        defense = Et.SubElement(card, 'defense', guard='0')
+    else:
+        defense = Et.SubElement(card, 'defense', guard='1')
+        defense.text = element['defense'][1]
+    level = Et.SubElement(card, 'level')
+    if element['level'] != 'None':
+        level.text = element['level']
+    role = Et.SubElement(card, 'role')
+    if element['role'] != 'None':
+        role.text = element['role']
+    setqty = Et.SubElement(card, 'setqty')
+    if element['setqty'] != 'None':
+        setqty.text = element['setqty']
 
-FILE = open('game.dat', 'wb')
-FILE.write(bytes(data, encoding='utf-8'))
-FILE.write(pic)
-FILE.close()
+
+doc = minidom.parseString(Et.tostring(root, encoding='utf-8'))
+tree = Et.ElementTree(Et.fromstring(doc.toprettyxml(encoding='utf-8')))
+tree.write('data.xml', encoding='utf-8', xml_declaration=True)
+
+
+
+
